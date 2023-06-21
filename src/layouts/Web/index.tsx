605f8d09c832c23b3dc9376491eee70e7dad9fcc
quocan0301 <ananpro03103@gmail.com>
2023-06-21 18:15:33 +0700

    header

---
+++ src/layouts/Web/index.tsx
@@ -0,0 +1,23 @@
+import { FC, PropsWithChildren, memo } from 'react';
+import './index.scss';
+import Header from './Header';
+
+interface Props {
+    title?: string;
+    subTitle?: string;
+}
+
+const MainLayout: FC<PropsWithChildren<Props>> = memo(
+    
+    ({ children, title, subTitle }) => {
+        return( 
+            <div>
+            <Header />
+              {children}
+          </div>
+        );
+    }
+);
+MainLayout.displayName = 'MainLayout';
+
+export default MainLayout;
