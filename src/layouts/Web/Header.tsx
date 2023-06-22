8065c733844c09a7c35e1dbfd00e98af17ffd3c3
quocan0301 <ananpro03103@gmail.com>
2023-06-22 13:25:08 +0700

    layout for resetpw

--- src/layouts/Web/Header.tsx
+++ src/layouts/Web/Header.tsx
@@ -1,29 +1,49 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
+const isLoggedIn: boolean = false;
 const Header: React.FC = () => {
-    return (
-        <header className="header">
-        <div className="header-content">
-        <div className="header-left">
-            <span className="logo"><strong>AUTO</strong>POST</span>
-            <div className="searchbar">
-            <input type="text" placeholder="Tìm kiếm" />
-            </div>
-        </div>
-        <div className="header-center"> 
-          <a href="/"> TRANG CHỦ
-          </a>
-          <a href="/"> BÀI VIẾT
-          </a>
-        </div>
-        <div className="header-right">
-            <button className="create-post-button">Đăng bài</button>
-            <span>Hello, <a href="/">Quốc An</a>!</span>
-        </div>
-        </div>
-    </header>
-    );
+    if (isLoggedIn) {
+        return (
+            <header className="header">
+                <div className="header-content">
+                    <div className="header-left">
+                        <span className="logo">
+                            <strong>AUTO</strong>POST
+                        </span>
+                        <div className="searchbar">
+                            <input type="text" placeholder="Tìm kiếm" />
+                        </div>
+                    </div>
+                    <div className="header-center">
+                        <a href="/"> TRANG CHỦ</a>
+                        <a href="/post"> BÀI VIẾT</a>
+                    </div>
+                    <div className="header-right">
+                        <button className="create-post-button">Đăng bài</button>
+                        <span>
+                            Hello, <a href="/">Quốc An</a>!
+                        </span>
+                    </div>
+                </div>
+            </header>
+        );
+    } else {
+        return (
+            <header className="header">
+                <div className="header-content">
+                    <div className="header-left">
+                        <span className="logo">
+                            <strong>AUTO</strong>POST
+                        </span>
+                    </div>
+                    <div className="header-center"></div>
+                    <div className="header-right">
+                        <button className="login">Đăng nhập</button>
+                    </div>
+                </div>
+            </header>
+        );
+    }
 };
 
 export default Header;
