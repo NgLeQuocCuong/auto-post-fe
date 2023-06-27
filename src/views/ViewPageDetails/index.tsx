b3d7c8a647c742fdfead857e2209fe39b4950b9e
quocan0301 <ananpro03103@gmail.com>
2023-06-27 15:47:10 +0700

    fixed api

--- src/views/ViewPageDetails/index.tsx
+++ src/views/ViewPageDetails/index.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import './style.scss';
 import userService from 'services/userService';
 import { useNavigate } from 'react-router-dom';
-import { getToken } from 'reducers/token/function';
+import { useToken } from 'reducers/token/function';
 import routeConstants from 'route/routeConstant';
 import { FC } from 'react';
 interface Props {
@@ -13,40 +13,39 @@ interface Props {
     uid: string;
 }
 
-const Wrapper: FC<Props> = memo(uid => {
+const Wrapper: FC<Props> = memo(({ uid }) => {
     const navigate = useNavigate();
     const [postType, setpostType] = useState('');
     const [createAt, setcreateAt] = useState('');
     const [content, setContent] = useState('');
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
-    useEffect(function effectFunction() {
-        const token = getToken();
+    const token = useToken();
+    useEffect(() => {
         if (token) {
-            (async () => {
-                const response = await userService.getdetails(
-                    'b8e856b3-e41c-481b-b565-8463a0889dd1'
-                );
-                if (response.isSuccess) {
-                    setpostType(await response.data.postType);
-                    setcreateAt(await response.data.createdAt);
-                    setContent(await response.data.content);
-                    const datetime = new Date(createAt);
-                    if (!isNaN(datetime.getTime())) {
-                        const dt = datetime.toISOString().split('T')[0];
-                        const parts = dt.split('-');
-                        setDate(`${parts[2]}/${parts[1]}/${parts[0]}`);
-                        setTime(
-                            datetime.toLocaleTimeString([], {
-                                hour: '2-digit',
-                                minute: '2-digit',
-                            })
-                        );
+            userService
+                .getdetails('b8e856b3-e41c-481b-b565-8463a0889dd1')
+                .then(res => {
+                    if (res.isSuccess) {
+                        setpostType(res.data.postType);
+                        setcreateAt(res.data.createdAt);
+                        setContent(res.data.content);
+                        const datetime = new Date(createAt);
+                        if (!isNaN(datetime.getTime())) {
+                            const dt = datetime.toISOString().split('T')[0];
+                            const parts = dt.split('-');
+                            setDate(`${parts[2]}/${parts[1]}/${parts[0]}`);
+                            setTime(
+                                datetime.toLocaleTimeString([], {
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                })
+                            );
+                        }
+                    } else {
+                        navigate(routeConstants.MAINPAGE);
                     }
-                } else {
-                    navigate(routeConstants.MAINPAGE);
-                }
-            })();
+                });
         } else {
             navigate(routeConstants.LOGIN);
         }
