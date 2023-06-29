dd7a550fcfa5001e408ec00b026ee5bc8f7d10c6
minhphucvien <vienphuc2511@gmail.com>
2023-06-30 00:29:35 +0700

    fix: Move An's services to postService

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,7 +1,7 @@
 import ApiBase from 'modules/apis/apiBase';
 
 const _POST_PATH = '/posts';
-
+const _POST_MANAGEMENT_PATH = '/post-management';
 class PostService extends ApiBase {
     filterPosts = (params: {
         page?: number;
@@ -20,6 +20,15 @@ class PostService extends ApiBase {
     removePost = (params: { uid: string }) => {
         const url = `${_POST_PATH}/${params.uid}/remove`;
         const res = this.post(url);
+        return res;
+    };
+    getDetails = (uid: string) => {
+        const url = `${_POST_PATH}/${uid}/detail`;
+        return this.get(url);
+    };
+    getManagementDetails = (uid: string) => {
+        const url = `${_POST_PATH}${_POST_MANAGEMENT_PATH}/${uid}/detail`;
+        const res = this.get(url);
         return res;
     };
 }
