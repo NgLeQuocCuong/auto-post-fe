274611fa21217b80bcbe852159728825e531b6aa
caohanh1502 <caohanh1502@gmail.com>
2023-07-03 13:40:26 +0700

    fix: fix facebook login, get avatar, remove username

--- src/views/UserSettings/Inner.jsx
+++ src/views/UserSettings/Inner.jsx
@@ -1,6 +1,5 @@
 import { Button, Form, Typography, Space, List, Col, Row, Image } from 'antd';
 import { memo } from 'react';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 import WebLayout from 'layouts/Web/WebLayout';
 import moment from 'moment';
 import './index.scss';
@@ -14,14 +13,11 @@ const Inner = memo(
         handleLinkFacebook,
         handleUnlinkFacebook,
         facebookProcessing,
-        handleLinkZalo,
-        handleUnlinkZalo,
-        zaloProcessing,
+        // TODO: Uncomment this when LinkedIn is ready
+        // handleLinkLinkedin,
+        // handleUnlinkLinkedin,
+        // LinkedinProcessing,
     }) => {
-        const appId = '283990164019980';
-        const facebookPermissions =
-            'public_profile,publish_to_groups,pages_manage_metadata,pages_manage_posts,pages_manage_engagement,pages_show_list';
-        const facebookGetFields = 'name,email,picture,groups.limit(100)';
         return (
             <WebLayout title="User Settings">
                 <Row className="user-settings-form">
@@ -29,20 +25,15 @@ const Inner = memo(
                         <Image
                             width={94}
                             height={94}
-                            src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSSlr2_vaXAWXtDCZ627ItaZLtIXDXSDOA60uLfvcBnhnPnBCXs"
-                            className="user-settings-form__avatar--round"
+                            src={userInfo.avatar}
+                            className="user-settings-form__avatar--round user-settings-form__avatar--fit"
+                            alt="Avatar"
                         />
                     </Col>
                     <Col span={18}>
                         <Form layout="vertical">
                             <Form.Item>
-                                <Typography.Text
-                                    component="div"
-                                    className="user-settings-form__title"
-                                >
-                                    {userInfo.username}
-                                </Typography.Text>
-                                <Space>
+                                <Space className="mt-3">
                                     <NavLink to={routeConstants.USER_UPDATE}>
                                         <Button
                                             size="large"
@@ -94,48 +85,29 @@ const Inner = memo(
                                                 ''
                                             )}
                                         </Typography.Text>
-                                        <FacebookLogin
-                                            appId={appId}
-                                            version="17.0"
-                                            scope={facebookPermissions}
-                                            fields={facebookGetFields}
-                                            callback={handleLinkFacebook}
-                                            render={renderProps => (
-                                                <Button
-                                                    type="link"
-                                                    className="user-settings-form__btn--padding-x-0"
-                                                    danger={
-                                                        userInfo.facebookStatus
-                                                    }
-                                                    onClick={
-                                                        userInfo.facebookStatus
-                                                            ? handleUnlinkFacebook
-                                                            : renderProps.onClick
-                                                    }
-                                                    onChange={
-                                                        userInfo.facebookStatus
-                                                            ? null
-                                                            : renderProps.callback
-                                                    }
-                                                    disabled={
-                                                        renderProps.isProcessing ||
-                                                        facebookProcessing
-                                                    }
-                                                >
-                                                    {renderProps.isProcessing ||
-                                                    facebookProcessing
-                                                        ? 'Đang xử lý...'
-                                                        : userInfo.facebookStatus
-                                                        ? 'Hủy liên kết Facebook'
-                                                        : 'Liên kết Facebook'}
-                                                </Button>
-                                            )}
-                                        />
+                                        <Button
+                                            type="link"
+                                            className="user-settings-form__btn--padding-x-0"
+                                            danger={userInfo.facebookStatus}
+                                            onClick={
+                                                userInfo.facebookStatus
+                                                    ? handleUnlinkFacebook
+                                                    : handleLinkFacebook
+                                            }
+                                            disabled={facebookProcessing}
+                                        >
+                                            {facebookProcessing
+                                                ? 'Đang xử lý...'
+                                                : userInfo.facebookStatus
+                                                ? 'Hủy liên kết Facebook'
+                                                : 'Liên kết Facebook'}
+                                        </Button>
                                     </List.Item>
-                                    <List.Item>
+                                    {/* TODO: Uncomment when LinkedIn is ready */}
+                                    {/* <List.Item>
                                         <Typography.Text className="user-settings-form__list--icon-group">
-                                            Zalo
-                                            {userInfo.zaloStatus ? (
+                                            Linkedin
+                                            {userInfo.LinkedinStatus ? (
                                                 <CheckIcon />
                                             ) : (
                                                 ''
@@ -144,20 +116,20 @@ const Inner = memo(
                                         <Button
                                             type="link"
                                             className="user-settings-form__btn--padding-x-0"
-                                            danger={userInfo.zaloStatus}
+                                            danger={userInfo.LinkedinStatus}
                                             onClick={
-                                                userInfo.zaloStatus
-                                                    ? handleUnlinkZalo
-                                                    : handleLinkZalo
+                                                userInfo.LinkedinStatus
+                                                    ? handleUnlinkLinkedin
+                                                    : handleLinkLinkedin
                                             }
                                         >
-                                            {zaloProcessing
+                                            {LinkedinProcessing
                                                 ? 'Đang xử lý...'
-                                                : userInfo.zaloStatus
-                                                ? 'Hủy liên kết Zalo'
-                                                : 'Liên kết Zalo'}
+                                                : userInfo.LinkedinStatus
+                                                ? 'Hủy liên kết Linkedin'
+                                                : 'Liên kết Linkedin'}
                                         </Button>
-                                    </List.Item>
+                                    </List.Item> */}
                                 </List>
                             </Form.Item>
                             <Form.Item className="user-settings-form__label--padding-bottom-0">
--- src/views/UserSettings/index.jsx
+++ src/views/UserSettings/index.jsx
@@ -1,5 +1,5 @@
 import Message from 'components/Message';
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback, useState, useEffect } from 'react';
 import { setUserProfile, useUserProfile } from 'reducers/profile/function';
 import { setFacebookToken } from 'reducers/token/function';
 import userService from 'services/userService';
@@ -8,13 +8,47 @@ import Inner from 'views/UserSettings/Inner';
 const Wrapper = memo(() => {
     const [userInfo, setUserInfo] = useState(useUserProfile());
     const [facebookProcessing, setFacebookProcessing] = useState(false);
-    const [zaloProcessing, setZaloProcessing] = useState(false);
-    const handleLinkFacebook = useCallback(
-        async data => {
-            setFacebookProcessing(true);
-            if (data.accessToken) {
+    const [LinkedinProcessing, setLinkedinProcessing] = useState(false);
+
+    const FACEBOOK_OAUTH_CLIENT_ID = '283990164019980';
+    const FACEBOOK_PERMISSIONS =
+        'public_profile,publish_to_groups,pages_manage_metadata,pages_manage_posts,pages_manage_engagement,pages_show_list';
+    //const facebookGetFields = 'name,email,picture,groups.limit(100)';
+    const initFacebookSdk = useCallback(() => {
+        // wait for facebook sdk to initialize before starting the react app
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: FACEBOOK_OAUTH_CLIENT_ID,
+                cookie: true,
+                xfbml: true,
+                version: 'v17.0',
+            });
+        };
+
+        // load facebook sdk script
+        (function (d, s, id) {
+            var js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+    }, [FACEBOOK_OAUTH_CLIENT_ID]);
+
+    useEffect(() => {
+        initFacebookSdk();
+    }, [initFacebookSdk]);
+
+    const saveTokenFacebook = useCallback(
+        async responseFB => {
+            if (responseFB.status === 'connected') {
+                const accessToken = responseFB.authResponse.accessToken;
                 const response = await userService.linkFacebook({
-                    token: data.accessToken,
+                    token: accessToken,
                 });
                 if (response.isSuccess) {
                     setUserInfo(prevState => ({
@@ -22,18 +56,28 @@ const Wrapper = memo(() => {
                         facebookStatus: true,
                     }));
                     setUserProfile(userInfo);
-                    setFacebookToken(data.accessToken);
+                    setFacebookToken(accessToken);
                     Message.sendSuccess('Liên kết Facebook thành công!');
                 }
                 setFacebookProcessing(false);
                 return response;
             } else {
-                Message.sendError('Liên kết Facebook không thành công.');
+                Message.sendError('Liên kết Facebook không thành công!');
+                setFacebookProcessing(false);
             }
-            setFacebookProcessing(false);
         },
         [userInfo]
     );
+
+    const handleLinkFacebook = useCallback(async () => {
+        setFacebookProcessing(true);
+        await window.FB.login(
+            responseFB => {
+                saveTokenFacebook(responseFB);
+            },
+            { scope: FACEBOOK_PERMISSIONS }
+        );
+    }, [saveTokenFacebook]);
 
     const handleUnlinkFacebook = useCallback(async () => {
         setFacebookProcessing(true);
@@ -51,43 +95,43 @@ const Wrapper = memo(() => {
         return response;
     }, [userInfo]);
 
-    const handleLinkZalo = useCallback(
+    const handleLinkLinkedin = useCallback(
         async data => {
-            setZaloProcessing(true);
-            // const response = await userService.linkZalo({oathCode: data.oathCode});
+            setLinkedinProcessing(true);
+            // const response = await userService.linkLinkedin({oathCode: data.oathCode});
             const response = { ...data, isSuccess: false }; // Mock response
             if (response.isSuccess) {
                 setUserInfo(prevState => ({
                     ...prevState,
-                    zaloStatus: true,
+                    LinkedinStatus: true,
                 }));
                 setUserProfile(userInfo);
-                // setZaloToken(data.oathCode);
-                Message.sendSuccess('Liên kết Zalo thành công!');
+                // setLinkedinToken(data.oathCode);
+                Message.sendSuccess('Liên kết Linkedin thành công!');
             } else {
                 Message.sendWarning(
                     'Chức năng đang phát triển. Vui lòng thử lại sau.'
                 );
             }
-            setZaloProcessing(false);
+            setLinkedinProcessing(false);
             return response;
         },
         [userInfo]
     );
 
-    const handleUnlinkZalo = useCallback(async () => {
-        setZaloProcessing(true);
-        const response = await userService.unlinkZalo();
+    const handleUnlinkLinkedin = useCallback(async () => {
+        setLinkedinProcessing(true);
+        const response = await userService.unlinkLinkedin();
         if (response.isSuccess) {
             setUserInfo(prevState => ({
                 ...prevState,
-                zaloStatus: false,
+                LinkedinStatus: false,
             }));
             setUserProfile(userInfo);
-            // setZaloToken('');
-            Message.sendInfo('Hủy liên kết Zalo thành công!');
+            // setLinkedinToken('');
+            Message.sendInfo('Hủy liên kết Linkedin thành công!');
         }
-        setZaloProcessing(false);
+        setLinkedinProcessing(false);
         return response;
     }, [userInfo]);
 
@@ -97,9 +141,9 @@ const Wrapper = memo(() => {
             handleLinkFacebook={handleLinkFacebook}
             handleUnlinkFacebook={handleUnlinkFacebook}
             facebookProcessing={facebookProcessing}
-            handleLinkZalo={handleLinkZalo}
-            handleUnlinkZalo={handleUnlinkZalo}
-            zaloProcessing={zaloProcessing}
+            handleLinkLinkedin={handleLinkLinkedin}
+            handleUnlinkLinkedin={handleUnlinkLinkedin}
+            LinkedinProcessing={LinkedinProcessing}
         />
     );
 });
