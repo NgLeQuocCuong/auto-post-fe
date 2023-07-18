3cfa1a80a44c1f055b9f7ad177149f033f5ea6cf
minhphucvien <vienphuc2511@gmail.com>
2023-07-18 23:47:11 +0700

    fix: remove CounterTag from tag array, refactor TagSelect

--- src/views/AllPosts/Dropdown/components/TagSelect.tsx
+++ src/views/AllPosts/Dropdown/components/TagSelect.tsx
@@ -4,44 +4,33 @@ import type { InputRef } from 'antd';
 import { Button, Space, Input, Tag, Tooltip } from 'antd';
 import { ChangeEvent } from 'react';
 import { useSearchParams } from 'react-router-dom';
+
 interface TagSelectProps {
     onChange: (tags: string[]) => void;
 }
 
 const TagSelect: FC<TagSelectProps> = memo(({ onChange }) => {
-    const [tags, setTags] = useState(['0/5']);
+    const [tags, setTags] = useState<string[]>([]);
     const [inputVisible, setInputVisible] = useState(false);
     const [inputValue, setInputValue] = useState('');
     const [editInputIndex, setEditInputIndex] = useState(-1);
     const [editInputValue, setEditInputValue] = useState('');
     const inputRef = useRef<InputRef>(null);
     const editInputRef = useRef<InputRef>(null);
-    useEffect(() => {
-        if (inputVisible) {
-            inputRef.current?.focus();
-        }
-    }, [inputVisible]);
-
     const [searchParams] = useSearchParams();
     useEffect(() => {
         const postTypesFromUrl = searchParams.getAll('postType');
         if (postTypesFromUrl.length > 0) {
             const postTypes = postTypesFromUrl.join(',').split(',');
-            const counterTag = `${postTypes.length}/5`;
-            setTags(() => [counterTag, ...postTypes]);
+            setTags(postTypes);
         }
     }, [searchParams]);
 
     const handleClose = useCallback(
         (removedTag: string) => {
-            if (removedTag !== tags[0]) {
-                const newTags = tags.filter(tag => tag !== removedTag);
-                const tagCount = newTags.length - 1;
-                const counterTag = `${tagCount}/5`;
-                newTags[0] = counterTag;
-                setTags(newTags);
-                onChange(newTags.slice(1));
-            }
+            const newTags = tags.filter(tag => tag !== removedTag);
+            setTags(newTags);
+            onChange(newTags);
         },
         [onChange, tags]
     );
@@ -59,18 +48,11 @@ const TagSelect: FC<TagSelectProps> = memo(({ onChange }) => {
 
     const handleInputConfirm = useCallback(() => {
         if (inputValue && tags.indexOf(inputValue) === -1) {
-            const newTags = [...tags, inputValue];
-            setTags(newTags);
-            const tagCount = newTags.length - 1;
-            const counterTag = `${tagCount}/5`;
-            newTags[0] = counterTag;
-            setTags(newTags);
-            onChange(newTags.slice(1));
+            setTags(prevTags => [...prevTags, inputValue]);
+            setInputValue('');
         }
-
         setInputVisible(false);
-        setInputValue('');
-    }, [inputValue, onChange, tags]);
+    }, [inputValue, tags]);
 
     const handleEditInputChange = useCallback(
         (e: ChangeEvent<HTMLInputElement>) => {
@@ -84,12 +66,12 @@ const TagSelect: FC<TagSelectProps> = memo(({ onChange }) => {
         newTags[editInputIndex] = editInputValue;
         setTags(newTags);
         setEditInputIndex(-1);
-        setInputValue('');
-        onChange(newTags.slice(1));
+        onChange(newTags);
     }, [editInputIndex, editInputValue, onChange, tags]);
 
     return (
         <Space size={[0, 8]} wrap>
+            <div className="filter-tag-counter">{tags.length}/5</div>
             {tags.map((tag, index) => {
                 if (editInputIndex === index) {
                     return (
@@ -97,6 +79,7 @@ const TagSelect: FC<TagSelectProps> = memo(({ onChange }) => {
                             ref={editInputRef}
                             key={tag}
                             size="small"
+                            value={editInputValue}
                             onChange={handleEditInputChange}
                             onBlur={handleEditInputConfirm}
                             onPressEnter={handleEditInputConfirm}
@@ -107,20 +90,16 @@ const TagSelect: FC<TagSelectProps> = memo(({ onChange }) => {
                 const tagElem = (
                     <Tag
                         key={tag}
-                        closable={index !== 0}
-                        style={{
-                            userSelect: 'none',
-                        }}
+                        closable
+                        style={{ userSelect: 'none' }}
                         className="filter-tag-select"
                         onClose={() => handleClose(tag)}
                     >
                         <span
                             onDoubleClick={e => {
-                                if (index !== 0) {
-                                    setEditInputIndex(index);
-                                    setEditInputValue(tag);
-                                    e.preventDefault();
-                                }
+                                setEditInputIndex(index);
+                                setEditInputValue(tag);
+                                e.preventDefault();
                             }}
                         >
                             {isLongTag ? `${tag.slice(0, 10)}...` : tag}
@@ -151,7 +130,7 @@ const TagSelect: FC<TagSelectProps> = memo(({ onChange }) => {
                     type="default"
                     size="middle"
                     onClick={showInput}
-                    disabled={tags.length >= 6} //5 tags + 1 counter tag
+                    disabled={tags.length >= 5}
                 >
                     <PlusOutlined /> Thêm
                 </Button>
